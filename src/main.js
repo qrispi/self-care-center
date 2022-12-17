3bd28bf7b922bf187d1e61c731b6589a616b5281
Christopher Baum <christopherabaum@gmail.com>
2022-12-16 21:18:47 -0600

    Refactor main.js, simplify functions and remove unneccesary lines and vars

--- src/main.js
+++ src/main.js
@@ -1,38 +1,24 @@
 var receiveMsgButton = document.getElementById('receive-button')
-// var yogaMonk = document.getElementById('dude')
 var affirmSelect = document.getElementById('affirm-button')
 var mantraSelect = document.getElementById('mantra-button')
 var messageBox = document.querySelector('.message-section')
-// var userForm = document.querySelector('form')
 
-// var userSelectAffirm = affirmSelect.value
-// var userSelectMantra = mantraSelect.value
-
-var selection
-
-// userForm.addEventListener('submit', retrieveSelection)
 receiveMsgButton.addEventListener('click', retrieveSelection)
 
 function retrieveSelection(event) {
     event.preventDefault()
-    console.log('affirm select', affirmSelect.checked)
-    console.log('mantra select', mantraSelect.checked)
     if (affirmSelect.checked) {
-        selection = affirmations
-        displayNewMsg()
+        displayNewMsg(affirmations)
     } else if (mantraSelect.checked) {
-        selection = mantras
-        displayNewMsg()
+        displayNewMsg(mantras)
     } else {
         alert('Please select which type of message you would like to receive')
     }
 }
 
-
-function displayNewMsg() {
-    console.log('var selection = ', selection)
+function displayNewMsg(msgArray) {
     messageBox.innerHTML = `
-    <p>${getRandomMessage(selection)}</p>
+    <p>${getRandomMessage(msgArray)}</p>
     `
 }
 
